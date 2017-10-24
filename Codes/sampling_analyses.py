7b3eee570644b51752fef77d1e73d0870689c104
triveditarun <tarun02671@gmail.com>
2017-10-24 18:50:02 +0530

    V3

--- Codes/sampling_analyses.py
+++ Codes/sampling_analyses.py
@@ -8,6 +8,9 @@ Created on Tue Oct 24 17:59:35 2017
 import pandas as pd
 import numpy as np
 
+read = 'D://Learning/basic_analyses/Data/'
+write = 'D://Learning/basic_analyses/Analysis/'
+
 def random_sampling_distri(num_obs, seeds):
     summary = pd.DataFrame()
     for obs in xrange(0, num_obs, 500):
@@ -19,9 +22,11 @@ def random_sampling_distri(num_obs, seeds):
             np.random.seed(seed + 1)
             df['rand_num'] = np.random.random(df.shape[0])
             df['sample'] = np.where(df['rand_num'] <= 0.63, 'dev', 'val')
-            summ = df.groupby(['sample']).agg({'ID': np.size, 'event': np.mean})
+            summ = df.groupby(['sample']).agg({'ID': np.size, 'event': np.mean}).reset_index()
             summ['num_ids'] = summ['ID'].sum()
             summary = summary.append(summ)
+            summary.sort_values(by = ['sample', 'ID'], ascending = [True, True], inplace = True)
     return summary
 
 summary = random_sampling_distri(5000, 250)
+summary.to_excel(write + 'sampling_distribution_analysis.xlsx')
