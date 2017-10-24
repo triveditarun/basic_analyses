41f52e552f5868bace27a22ee62403573c665030
triveditarun <tarun02671@gmail.com>
2017-10-24 18:29:15 +0530

    V1

---
+++ Codes/sampling_analyses.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 17:59:35 2017
+
+@author: LK
+"""
+
+imm
---
+++ Codes/web_scraping.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 16:44:46 2017
+
+@author: LK
+"""
+
+from urllib import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 
