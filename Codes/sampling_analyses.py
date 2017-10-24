b55eb8e6a4ed6c5855501a9f1cab7934d3180488
triveditarun <tarun02671@gmail.com>
2017-10-24 18:30:35 +0530

    V1

--- Codes/sampling_analyses.py
+++ Codes/sampling_analyses.py
@@ -5,4 +5,24 @@ Created on Tue Oct 24 17:59:35 2017
 @author: LK
 """
 
-imm
+import pandas as pd
+import numpy as np
+
+def random_sampling_distri(num_obs, seeds):
+    summary = pd.DataFrame()
+    for obs in xrange(0, num_obs, 500):
+        for seed in xrange(0, seeds, 50):
+            df = pd.DataFrame(index = range(obs), columns = ['ID', 'event', 'sample'])
+            np.random.seed(seed)
+            df['rand_num'] = np.random.random(df.shape[0])
+            df['event'] = np.where(df['rand_num'] <= 0.12, 1, 0)
+            np.random.seed(seed + 1)
+            df['rand_num'] = np.random.random(df.shape[0])
+            df['sample'] = np.where(df['rand_num'] <= 0.63, 'dev', 'val')
+            summ = df.groupby(['sample']).agg({'ID': np.size, 'event': np.mean})
+            summary = summary.append(summ)
+    return summary
+
+summary = random_sampling_distri(5000, 250)
+    
+        
